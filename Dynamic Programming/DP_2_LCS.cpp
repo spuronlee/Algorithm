bda2b10ccb6fb27c2c3304502165b43d830c13b9
spuronlee <42567047+spuronlee@users.noreply.github.com>
2020-03-16 23:21:39 +0800

    Update DP_2_LCS.cpp

--- Dynamic Programming/DP_2_LCS.cpp
+++ Dynamic Programming/DP_2_LCS.cpp
@@ -1,57 +1,59 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
-#define NUM 51
-
+typedef pair<int,string> LCS;
+#define MAX 100
+int c[MAX][MAX];
+int b[MAX][MAX];
 string result("");
 
-void TraceBack(int i, int j, int s[NUM][NUM]){
+void LCS_output(int i,int j, string text){
+    if(i==0 || j==0) return;
+    if (b[i][j] == 1){
+        string in = string(1, text[i-1]);
+        result.insert(0, in);
+        LCS_output(i-1,j-1,text);
+    }
+    else if(b[i][j] == 2){
+        LCS_output(i-1,j,text);
+    }else{
+        LCS_output(i,j-1,text);
+    }
 
 
-    if(i==j){
-        result.insert(result.length(), "A");
-        result.insert(result.length(), to_string(i));
-    }
-    else{
-        result.insert(result.length(), "(");
-        TraceBack(i, s[i][j], s);
-        TraceBack(s[i][j]+1, j, s);
-        result.insert(result.length(), ")");
-    }
 }
 
+LCS longestCommonSubsequence(string text1, string text2) {
+    int m = text1.length();
+    int n = text2.length();
 
-string MarticChain(int * p, int n)
-{
-    int m[NUM][NUM];
-    int s[NUM][NUM];
-    for(int i = 1; i <= n; i++) m[i][i] = 0;
-
-    for(int r = 2; r <= n; r++)
-    {
-        for(int i = 1; i<= n-r+1; i++)
-        {
-            int j = i+r-1;
-            m[i][j] = m[i+1][j] + p[i-1]*p[i]*p[j];
-            s[i][j] = i;
-
-            for (int k = i+1; k<j; k++)
-            {
-                int t = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
-                if (t < m[i][j]){m[i][j] = t; s[i][j] = k;}
+    for (int i=0;i<=m;i++) c[i][0] = 0;
+    for (int i=0;i<=n;i++) c[0][i] = 0;
+    for (int i=1;i<=m;i++){
+        for (int j=1;j<=n;j++){
+            if (text1[i-1]==text2[j-1]){
+                c[i][j] = c[i-1][j-1]+1;
+                b[i][j] = 1;
+            }
+            else if (c[i-1][j] >= c[i][j-1]){
+                 c[i][j] = c[i-1][j];
+                 b[i][j] = 2;
+            }else{
+                 c[i][j] = c[i][j-1];
+                 b[i][j] = 3;
             }
         }
-     }
-    TraceBack(1, n, s);
-    return result;
+    }
+    LCS_output(m, n, text1);
+    return {c[m][n], result};
 }
-
 
 int main()
 {
-    int n = 6;
-    int p[n+1] = {50, 10, 40, 30, 5, 20 ,15};
-    string re = MarticChain(p, n);
-    cout << re << endl;
+    string text1("abcde");
+    string text2("ace");
+    LCS l = longestCommonSubsequence(text1, text2);
+    cout<<"The longest Common Subsequence is: "<<l.second<<", its length is:"<<l.first<<"."<<endl;
     return 0;
 }
